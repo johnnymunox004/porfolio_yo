7d25fd183e316e0ea0c99211d1a2e409c3af63e2
johnnymunox004 <135404054+johnnymunox004@users.noreply.github.com>
2025-03-09 10:35:44 -0500

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 export default function Contact() {
   const [formData, setFormData] = useState({
     name: '',
+    email: 'johnynm@gmail.com',
     message: ''
   });
 
